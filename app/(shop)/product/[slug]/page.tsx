33a6ba5bd6b5e63ebf4fada7058101821aa74192
Emanuel <emanuelediaz25@gmail.com>
2026-02-17 23:24:33 -0500

    hecho hasta el slideshow

--- app/(shop)/product/[slug]/page.tsx
+++ app/(shop)/product/[slug]/page.tsx
@@ -1,29 +1,38 @@
-import { QuantitySelector, SizeSelector } from "@/components";
+import { ProductMobileSlideshow, ProductSlideshow, QuantitySelector, SizeSelector } from "@/components";
 import { titleFont } from "@/config/fonts";
 import { initialData } from "@/seed/seed";
 import { notFound } from "next/navigation";
-
 
 export default async function Product({
   params,
 }: {
   params: Promise<{ slug: string }>;
 }) {
-
   const { slug } = await params;
 
-  const product = initialData.products.find(product => product.slug === slug);
+  const product = initialData.products.find((product) => product.slug === slug);
 
   if (!product) {
     notFound();
   }
   return (
-
     <div className="mt-5 mb-20 grid col-span-1 md:grid-cols-3 gap-3">
       {/* SlideShow */}
-      <div className="col-span-1 md:col-span-2">
 
-        hola
+      <div className="col-span-1 md:col-span-2">
+        {/* Mobile Slideshow */}
+        <ProductMobileSlideshow
+          images={product.images}
+          title={product.title ? product.title : ""}
+          className="block md:hidden"
+        />
+
+        {/* Descktop Slideshow */}
+        <ProductSlideshow
+          images={product.images}
+          title={product.title ? product.title : ""}
+          className="hidden md:block"
+        />
       </div>
       {/* Detalles */}
       <div className="col-span-1 px-5 ">
@@ -38,23 +47,16 @@ export default async function Product({
         />
         {/* Seletor de cantidad */}
 
-        <QuantitySelector
-          quantity={1}
-        />
+        <QuantitySelector quantity={1} />
 
         {/* Button */}
 
-        <button className="btn-primary my-5">
-          Agregar al carrito
-        </button>
+        <button className="btn-primary my-5">Agregar al carrito</button>
 
         {/* Descripcion */}
         <h3 className="font-bold text-sm">Descripcion</h3>
-        <p className="font-light">
-          {product.description}
-        </p>
+        <p className="font-light">{product.description}</p>
       </div>
     </div>
-
   );
 }
